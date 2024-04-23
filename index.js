1b5a8ce549fa43db4d6444057bf1ae32ae96b110
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-04-23 18:08:46 +0530

    step 2 add html to server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { createServer} from 'node:http'
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const app = express();
-let port = process.env.PORT || 3000;
 const server = createServer(app);
+const __dirname = fileURLToPath(import.meta.url);
+
+let port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.sendFile(join(__dirname, '../index.html'));
 })
 
 server.listen(port, () => {
