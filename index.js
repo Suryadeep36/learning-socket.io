3360833ff317899e37c38c7601f63174d1e8bb68
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-04-25 17:31:59 +0530

    step 4 emitting events and broadcasting them

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import { createServer} from 'node:http'
 import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-
+import { join } from 'node:path';
+import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
 const __dirname = fileURLToPath(import.meta.url);
-
+const io = new Server(server);
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, '../index.html'));
+})
+
+io.on('connection',(socket) =>{
+    console.log("User connected");
+    socket.on('chat message',(msg) => {
+        io.emit('chat message',msg)
+    })
+    socket.on('disconnect',() =>{
+        console.log("User disconnected");
+    })
 })
 
 server.listen(port, () => {
